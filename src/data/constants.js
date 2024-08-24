4932261479abe67036863880123935170c8ab57e
nadafathma <tonadafathma@gmail.com>
2024-08-24 13:52:59 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,273 @@
+export const Bio = {
+  name: "Nada Fathima",
+  roles: [
+    "Full Stack Developer",
+    "Frontend Developer",
+    "Wordpress Developer",
+    "Software Tester",
+    "Freelancer",
+  ],
+  description:
+    "I'm a passionate and flexible developer who loves learning new things. I focus on creating high-quality solutions and am always ready to tackle new challenges with a positive attitude. I'm excited to contribute to meaningful projects and achieve great results.",
+  github: "https://github.com/nadafathima",
+  resume:
+    "https://drive.google.com/file/d/14TAPJivFPlKhUq6nR8g27banlcljW5ym/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/nadafathimato/",
+  twitter: "https://x.com/NadaFathimato",
+  insta: "https://www.instagram.com/nadhaahh/",
+  facebook: "https://www.facebook.com/nada.fathimato",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://i.postimg.cc/XqsqY2kS/tailwind.png",
+      },
+
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image:
+          "https://i.postimg.cc/nLqNwKv4/ui.png",
+      },
+      {
+        name: "Wordpress",
+        image: "https://i.postimg.cc/ZYfmK3xP/wordpress.jpg",
+      }, 
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://i.postimg.cc/P5QRWfhy/express.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://i.postimg.cc/ydh71bYv/git.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://i.postimg.cc/pXKVqytg/postman.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://i.postimg.cc/xCgMkg63/figma.png",
+      },
+      {
+        name: "Selenium",
+        image:
+          "https://i.postimg.cc/ZYBrDpH7/selenium.png",
+      },
+      {
+        name: "JMeter",
+        image:
+          "https://i.postimg.cc/RFqKHK2r/jmeter.jpg",
+      },
+
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://kiyado.com/wp-content/uploads/2023/05/Kiyado-header-logo.png",
+    role: "Web Developer Intern",
+    company: "Kiyado Innovation.",
+    date: "May 2024 - July 2024",
+    desc: "Working on Frontend, Wordpress, and Software Testing.",
+    skills: [
+      "Frontend Development",
+      "Wordpress",
+      "Postman",
+      "JMeter",
+      "Selenium",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://i.postimg.cc/0285Gj1X/ihrd.png",
+    school: "IHRD college of Applied Science Kozhikode",
+    date: "2021 - 2024",
+    grade: "62%",
+    degree: "Bachelor of Computer Application",
+  },
+  {
+    id: 1,
+    img: "https://i.postimg.cc/MHtS30JP/gov.jpg",
+    school: "SNTHSS Chelannur",
+    date: "2019 - 2021",
+    grade: "81%",
+    degree: "XII, Science",
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/MHtS30JP/gov.jpg",
+    school: "GHSS Narikkuni",
+    date: "2017 - 2019",
+    grade: "98%",
+    degree: "SSLC ",
+  },
+];
+
+export const projects = [
+{
+    id: 0,
+    title: "Netflix Clone",
+    date: "July 2024",
+    description:
+      "My Netflix clone is a web application designed to replicate the sleek and user-friendly interface of the popular streaming platform. Built using HTML and CSS, it features a visually appealing layout with a responsive design that adapts seamlessly to any screen size. The clone includes dynamic sections for showcasing movie and TV show categories, creating an immersive browsing experience similar to Netflix. This project demonstrates my frontend development skills, particularly in crafting modern and interactive web layouts.",
+    image:
+      "https://i.postimg.cc/kX80nfP6/netflix.png",
+    tags: [
+      "HTML",
+      "CSS",
+    ],
+    github: "https://github.com/nadafathima/",
+    webapp: "",
+  },
+  {
+    id: 1,
+    title: "Mini E-commerce",
+    date: "Dec 2023",
+    description:
+      "My mini eCommerce app is a user-friendly web application that simulates an online shopping experience. Built with HTML, CSS, Bootstrap, and JavaScript, it features a clean interface, dynamic product listings, and responsive design. This project showcases my frontend development skills and ability to create functional, modern web applications.",
+    image:
+      "https://i.postimg.cc/Sxd602V2/ecommerce.png",
+    tags: ["HTML", "CSS", "Bootstrap", "Javascript"],
+    github: "https://github.com/nadafathima/",
+    webapp: "",
+  },
+
+  {
+    id: 2,
+    title: "Coffee Shop Website",
+    date: "May 2024",
+    description:
+      "My GrandCoffee website showcases a carefully crafted blend of aesthetics and functionality. Designed to captivate coffee enthusiasts, it offers a smooth and intuitive user interface, making it easy to explore different coffee varieties. This project reflects my ability to create clean, responsive designs while ensuring an enjoyable browsing experience across devices.",
+    image:
+      "https://i.postimg.cc/MTsnpVNN/coffee.png",
+    tags: ["HTML", "CSS", "Bootstrap"],
+    github: "https://github.com/nadafathima/",
+    webapp: "",
+  },
+  {
+    id: 3,
+    title: "News App",
+    date: "May 2024",
+    description:
+      "A dynamic news web application that fetches and displays the latest news articles using the News API. With a sleek UI and smooth navigation, this project demonstrates my proficiency in API integration and frontend design.",
+    image:
+      "https://i.postimg.cc/Px0Zb0QT/news.png",
+    tags: ["HTML", "CSS", "Bootstrap","React.js"],
+    github: "https://github.com/nadafathima/",
+    webapp: "",
+  },
+  {
+    id: 4,
+    title: "BMI Calculator",
+    date: "July 2024",
+    description:
+      "A responsive BMI calculator designed to quickly compute Body Mass Index based on user inputs. Developed using React, Bootstrap, and CSS, this tool highlights my skills in React development and styling.",
+    image:
+      "https://i.postimg.cc/k4QYLNYr/bmi.png",
+    tags: ["HTML", "CSS", "Bootstrap","React.js"],
+    github: "https://github.com/nadafathima/",
+    webapp: "",
+  },
+  {
+    id: 5,
+    title: "Simple Calculator",
+    date: "May 2024",
+    description:
+      "A basic and user-friendly calculator application built using JavaScript, HTML, and CSS. This project showcases my ability to create functional interfaces with clean and efficient code.",
+    image:
+      "https://i.postimg.cc/1RL4vGhR/calculator.png",
+    tags: ["HTML", "CSS", "Bootstrap","Javascript"],
+    github: "https://github.com/nadafathima/",
+    webapp: "",
+  },
+
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Studying" },
+  { year: 2019, text: "Search about Development" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Explore" },
+];
