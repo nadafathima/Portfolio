b09945aabf89be5f60a62f5176244b064e9e646f
Nada Fathima <145420412+nadafathima@users.noreply.github.com>
2025-11-10 12:25:03 +0530

    Update EducationCard.jsx

--- src/components/Cards/EducationCard.jsx
+++ src/components/Cards/EducationCard.jsx
@@ -115,15 +115,15 @@ const Date = styled.div`
     @media only screen and (max-width: 768px){
         font-size: 10px;
     }
-`
+// `
 
-const Grade = styled.div`
-    font-size: 14px;
-    font-weight: 500;
-    color: ${({ theme }) => theme.text_secondary + 99};
-    @media only screen and (max-width: 768px){
-        font-size: 12px;
-    }
+// const Grade = styled.div`
+//     font-size: 14px;
+//     font-weight: 500;
+//     color: ${({ theme }) => theme.text_secondary + 99};
+//     @media only screen and (max-width: 768px){
+//         font-size: 12px;
+//     }
 `
 
 
@@ -139,7 +139,7 @@ const EducationCard = ({ education }) => {
                     <Date>{education.date}</Date>
                 </Body>
             </Top>
-            <Grade><b>Grade: </b>{education.grade}</Grade>
+            // <Grade><b>Grade: </b>{education.grade}</Grade>
             <Description>
                 <Span>{education.desc}</Span>
             </Description>
